a22709caed76d86194adc404c023ccdcbd871a99
Messias De Carvalho <messias.d.carvalho@hotmail.com>
2025-04-03 15:08:25 -0300

    update

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -4,8 +4,10 @@ class DropBoxController {
 
         this.btnSendFileEl = document.querySelector("#btn-send-file");
         this.InputFilesEl = document.querySelector("#files");
-        this.snackModalEl = document.querySelector("#react-snackbar-root")
-
+        this.snackModalEl = document.querySelector("#react-snackbar-root");
+        this.progressBarEl = this.snackModalEl.querySelector(".mc-progress-bar-fg")
+        this.nameFileEl = this.snackModalEl.querySelector(".filename")
+        this.timeLeftEl = this.snackModalEl.querySelector(".timeleft")
         this.initButtons();
     }
 
@@ -16,7 +18,69 @@ class DropBoxController {
 
         this.InputFilesEl.addEventListener('change', event=>{
             console.log(event.target.files);
+            this.uploadTask(event.target.files);
             this.snackModalEl.style.display = 'block';
         })
+    }
+
+    uploadTask(files){
+        let promises = [];
+        //files is as collection, because I had set to 
+        // receive mutiple files, so to make a foreach we need to convert the collection to an array
+        [...files].forEach(file=>{
+            promises.push(new Promise((resolve,reject)=>{
+                let ajax = new XMLHttpRequest();
+                ajax.open('POST','/upload');
+                ajax.onload = event=>{
+                    try{
+       
+                        resolve(JSON.parse(ajax.responseText));
+
+                    }catch(e){
+                        reject(e);
+                    }
+                }
+
+                ajax.onerror = event=>{
+                    reject(event);  
+                }
+
+                ajax.upload.onprogress = event=>{
+                    this.uploadProgress(event, file)
+                    console.log(event)
+                    
+                }
+                let formData = new FormData();
+                
+                
+
+                formData.append('input-file',file);
+                
+                this.startUploadTime = Date.now();
+
+                ajax.send(formData);
+
+            }))
+        })
+
+
+        return Promise.all(promises);
+    }
+
+    uploadProgress(event, file){
+       
+        let timeSpent = Date.now() - this.startUploadTime;
+        let loaded = event.loaded;
+        let total = event.total;
+        let percent = parseInt((loaded/total)*100);
+        let timeleft =((100-percent)*timeSpent) / percent;
+        this.progressBarEl.style.width = `${percent}%`
+
+        this.nameFileEl.innerHTML = file.name;
+    
+        console.log((timeleft))
+
+        console.log(timeSpent)
+
     }
 }
